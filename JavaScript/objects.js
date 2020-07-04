9dc17e9b64812c1c9a3b42f481d69ae6c730773e
Phaneendra Sai Kasanur <60728990+phaneendraKasanur@users.noreply.github.com>
2020-07-04 22:02:50 +0530

    exercise_7

---
+++ JavaScript/objects.js
@@ -0,0 +1,30 @@
+var userDetails = {
+    userName: "sample",
+    passWord: "1234"
+}
+
+userDetails.passWord;
+
+var database = [{
+        userName: "sample",
+        passWord: "1234"
+    },
+    {
+        userName: "tesst",
+        passWord: "12345"
+    }
+]
+
+var newsfeed = [{
+        username: "sample",
+        timeline: "So tired from all that learning!"
+    },
+    {
+        username: "tesst",
+        timeline: "Javascript is sooooo cool!"
+    },
+    {
+        username: "sampke",
+        timeline: "Javascript is preeetyy cool!"
+    }
+];
